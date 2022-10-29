845bf0bbb6532c44744c33d16e5889b203100bc2
Mandvi Alreja <mandvialreja93@gmail.com>
2022-10-29 16:46:58 -0700

    add likes part 2

---
+++ Webpages/Likes_part2/script.js
@@ -0,0 +1,24 @@
+var count= 9;
+var count1Element= document.querySelector(".count1");
+
+function increaseLikes1(){
+    count++;
+    count1Element.innerText= count + "like(s)";
+    console.log(count);
+}
+var count= 12;
+var count2Element= document.querySelector(".count2");
+
+function increaseLikes2(){
+    count++;
+    count2Element.innerText= count + "like(s)";
+    console.log(count);
+}
+var count= 9;
+var count3Element= document.querySelector(".count3");
+
+function increaseLikes3(){
+    count++;
+    count3Element.innerText= count + "like(s)";
+    console.log(count);
+}
